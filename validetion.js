21c67521ffe159b255a59d135a0f7a2213655bb2
Manuel-Freitashd <manuelfreitashd@gmail.com>
2020-06-08 11:53:13 +1400

    fist commit

---
+++ validetion.js
@@ -0,0 +1,13 @@
+ let  email = document.querySelectorAll("#email")
+ let  senha = document.querySelectorAll("#senha")
+
+ function validetion(){
+ 
+ const data =[
+     email.value,
+     senha.value
+    ]
+  
+    alert(data.email)
+    console.log(value)
+}
